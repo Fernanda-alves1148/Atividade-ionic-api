4398699c69d00c1efdf09988ed5faf73543f168e
fernandaAlvess <fernanda.alves1148@gmail.com>
2022-11-01 22:51:43 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  film: Observable<any>;
+  constructor(private router: Router, private http: HttpClient) {}
 
-  constructor() {}
+  ngOnInit(){
+    this.film = this.http.get('https://swapi.dev/api/films');
+  }
 
+abrirDetalhes(film){
+    this.router.navigateByUrl('/filme-detalhe/${filmId}');
+  }
 }
